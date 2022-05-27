d49ea44aa9300575e860e4cb8e1870c361bce619
iolantascheifel <iolanta.scheifel@gmail.com>
2022-05-27 22:35:23 +0200

    list of fav changed

--- components/ListOfFavourites.tsx
+++ components/ListOfFavourites.tsx
@@ -1,46 +1,50 @@
-import React from "react";
-import styled from "styled-components";
-import { PLACES } from "../dummy-data";
-import { RegionVisual } from "../types";
-import { PlaceCard } from "./PlaceCard";
-
-type Props = {
-  placeName: string;
-  placeImage: string;
-  location: string;
-  placeDesc: string;
-  regionVisual: RegionVisual;
+export const ListOfFavourites = () => {
+  return <div>ListOfFavourites</div>;
 };
 
-export const ListOfFavourites = ({
-  placeName,
-  placeDesc,
-  placeImage,
-  location,
-  regionVisual,
-}: Props) => {
-  const favouritePlaces = PLACES.filter(
-    (place) => place.placeName === placeName
-  );
+// import React from "react";
+// import styled from "styled-components";
+// import { PLACES } from "../dummy-data";
+// import { RegionVisual } from "../types";
+// import { PlaceCard } from "./PlaceCard";
 
-  return (
-    <div>
-      ListOfFavourites
-      {favouritePlaces.map((place) => (
-        <CardsContainer key={place.id}>
-          <PlaceCard
-            placeName={placeName}
-            placeImage={placeImage}
-            location={location}
-            placeDesc={placeDesc}
-            regionVisual={regionVisual}
-          />
-        </CardsContainer>
-      ))}
-    </div>
-  );
-};
+// type Props = {
+//   placeName: string;
+//   placeImage: string;
+//   location: string;
+//   placeDesc: string;
+//   regionVisual: RegionVisual;
+// };
 
-const CardsContainer = styled.div`
-  display: flex;
-`;
+// export const ListOfFavourites = ({
+//   placeName,
+//   placeDesc,
+//   placeImage,
+//   location,
+//   regionVisual,
+// }: Props) => {
+//   const favouritePlaces = PLACES.filter(
+//     (place) => place.placeName === placeName
+//   );
+
+//   return (
+//     <div>
+//       ListOfFavourites
+//       {favouritePlaces.map((place) => (
+//         <CardsContainer key={place.id}>
+//           <PlaceCard
+//             placeName={placeName}
+//             placeImage={placeImage}
+//             location={location}
+//             placeDesc={placeDesc}
+//             regionVisual={regionVisual}
+//           />
+//         </CardsContainer>
+//       ))}
+//     </div>
+//   );
+// };
+
+// const CardsContainer = styled.div`
+//   display: flex;
+// `;
